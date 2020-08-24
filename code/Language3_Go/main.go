dff85f8d17bdc1c3d3bf73a2ad6b76f381bdc8e7
Tanvi Pandit <panditanvi@gmail.com>
2020-08-24 11:34:58 -0400

    Code cleaning

--- code/Language3_Go/main.go
+++ code/Language3_Go/main.go
@@ -6,51 +6,55 @@ import (
 	"math/rand"
 	"time"
 )
- 
+ // Represents 2D field of cells 
 type Field struct {
-	s    [][]bool
-	w, h int
+	board[][]bool
+	width, height int
 }
- 
-func NewField(w, h int) Field {
-	s := make([][]bool, h)
+
+//Set new field with width and height 
+func NewField(width, height int) Field {
+	s := make([][]bool, height)
 	for i := range s {
-		s[i] = make([]bool, w)
+		s[i] = make([]bool, width)
 	}
-	return Field{s: s, w: w, h: h}
+	return Field{board: s, width: width, height: height}
 }
- 
+
+//Set the state b at location x,y on the board
 func (f Field) Set(x, y int, b bool) {
-	f.s[y][x] = b
+	f.board[y][x] = b
 }
- 
+
+// Return next state of the cell x, y at next time 
 func (f Field) Next(x, y int) bool {
-	on := 0
+	alive := 0
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
 			if f.State(x+i, y+j) && !(j == 0 && i == 0) {
-				on++
+				alive++
 			}
 		}
 	}
-	return on == 3 || on == 2 && f.State(x, y)
+	return alive == 3 || alive == 2 && f.State(x, y)
 }
  
 func (f Field) State(x, y int) bool {
 	for y < 0 {
-		y += f.h
+		y += f.height
 	}
 	for x < 0 {
-		x += f.w
+		x += f.width
 	}
-	return f.s[y%f.h][x%f.w]
+	return f.board[y%f.height][x%f.width]
 }
  
+ //Stores state of a round 
 type Life struct {
 	w, h int
 	a, b Field
 }
- 
+ // Returns a new game with with random initial state 
 func NewLife(w, h int) *Life {
 	a := NewField(w, h)
 	for i := 0; i < (w * h / 2); i++ {
@@ -63,6 +67,7 @@ func NewLife(w, h int) *Life {
 	}
 }
  
+// Advances game by one instant
 func (l *Life) Step() {
 	for y := 0; y < l.h; y++ {
 		for x := 0; x < l.w; x++ {
@@ -71,7 +76,8 @@ func (l *Life) Step() {
 	}
 	l.a, l.b = l.b, l.a
 }
- 
+
+ // Returns game board as a string
 func (l *Life) String() string {
 	var buf bytes.Buffer
 	for y := 0; y < l.h; y++ {
@@ -91,7 +97,7 @@ func main() {
 	l := NewLife(80, 15)
 	for i := 0; i < 300; i++ {
 		l.Step()
-		fmt.Print("\x0c")
+		fmt.Print("\x0c") // Clear the screen
 		fmt.Println(l)
 		time.Sleep(time.Second / 30)
 	}
